fd3e0347498f777f68a83060453a49f37314b07a
Chuhua Sun <chs090@ucsd.edu>
2020-10-27 19:34:26 -0700

    Deploy graphql to django and add root types Query and Mutation

---
+++ app/app/schema.py
@@ -0,0 +1,11 @@
+import graphene
+import tracks.schema
+
+class Query(tracks.schema.Query, graphene.ObjectType):
+    pass
+
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
